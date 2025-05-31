a6659d02a736275a9ce3738911874a2708b4cb43
Vera Khremli <lovet.post@gmail.com>
2025-05-31 13:36:32 +0300

    fix: change vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,8 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+   base: "/crypto/",
    plugins: [react()],
-
    server: {
       open: true,
       port: 3000,
